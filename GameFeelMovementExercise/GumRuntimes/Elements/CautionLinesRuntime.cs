5c57a2ac528b02dd8f16c82f02d6a6904362056d
David Antognoli <david.antognoli@gmail.com>
2022-10-20 10:26:03 -0500

    Initial project add with standard platformer template

---
+++ GameFeelMovementExercise/GumRuntimes/Elements/CautionLinesRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameFeelMovementExercise.GumRuntimes.Elements
+{
+    public partial class CautionLinesRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
---
+++ GameFeelMovementExercise/GumRuntimes/Elements/DividerHorizontalRuntime.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameFeelMovementExercise.GumRuntimes.Elements
+{
+    public partial class DividerHorizontalRuntime
+    {
+        partial void CustomInitialize () 
+        {
+        }
+    }
+}
